ef372cb8e54e41c08d8c791c865eb914b6cb0884
olagesin <feyisamuell@gmail.com>
2023-06-12 17:57:22 +0100

    create user

--- ParvigateUI/DTOs/GetTagDto.cs
+++ ParvigateUI/DTOs/GetTagDto.cs
@@ -1,4 +1,6 @@
-﻿namespace ParvigateUI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParvigateUI.DTOs
 {
     public class GetTagDto
     {
@@ -6,5 +8,26 @@
         public string SerialNumber { get; set; }
         public DateTime CreatedDate { get; set; }
         public bool IsActive { get; set; }
+    }
+
+    public class AddUserDto
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(30, ErrorMessage = "Name can be no larger than 30 characters")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(30, ErrorMessage = "Name can be no larger than 30 characters")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string RoleName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
     }
 }
